a9025173872d1f113c8b0c1a44543ceaaeb6967e
sk3els <32740021+sk3els@users.noreply.github.com>
2018-09-19 14:51:09 -0600

    Update client-api.py

--- client-api.py
+++ client-api.py
@@ -3,10 +3,12 @@ import requests, os
 requestJson = {'max_range': os.environ['MAX_RANGE']}
 functionUrl = os.environ['FUNCTION_URL']
 
+# validate max_range is in acceptable range
 if int(requestJson['max_range']) > 999 or int(requestJson['max_range']) < 0:
    print("Error: max_range must be between [0,999]")
    quit()
-   
+
+# make GET request to Google Cloud API service or otherwise specified function_url
 try:
    r = requests.get(functionUrl, params=requestJson)
    print(r.json())
